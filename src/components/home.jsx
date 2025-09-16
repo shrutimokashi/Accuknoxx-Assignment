8c238a33dfaceacbb20fcb57a888399ea8efd039
Shruti Dattatray Mokashi <66268047+shrutimokashi@users.noreply.github.com>
2025-09-16 12:22:36 +0530

    Add files via upload

---
+++ src/components/home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+    return (
+      <div className="w-full h-screen flex justify-center bg-[#f3f8c5] pt-20">
+        <p className="font-bold text-lg">Go to /dashboard to view the Dashboard</p>
+      </div>
+    );
+  };
+  
+  export default Home;
